8adebcde502beae93f85bd02a29b1f4075a3ef7b
Afreenmoham <afreenmoham@gmail.com>
2023-07-28 23:20:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "1. IN AN ABSOLUTE LOADING SCHEME,WHICH LOADER FUNCTION IS ACCOMPLISHED BY PROGRAMMER?",
+            "2. EXAMPLES OF SYSTEM PROGRAMS INCLUDES_________",
+            "3. TRANSLATOR FOR LOW LEVEL PROGRAMMING LANGUAGE WERE TERMED AS",
+            "4. A PROGRAM IN EXECUTION IS CALLED__________"
+    };
+
+    public static String choices[][]={
+            {"Linking","Allocation","Both (a) and (b)","Rellocation"},
+            {"Operating system of Computer","Trace program","Compiler","All of above"},
+            {"Assembler","Compiler","Linker","Loader"},
+            {"Process","Instruction","Procedure","Function"}
+
+    };
+
+    public static String correctAnswers[]={
+            "Both (a) and (b)",
+            "All of above",
+            "Assembler",
+            "Process"
+    };
+}
